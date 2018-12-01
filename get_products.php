42a6b3beeedf32f840e1239cdfd7d45c4276ed9c
kartlos diakonidze <kartlos99@yahoo.com>
2018-12-01 22:56:07 +0400

    upd_2

--- get_products.php
+++ get_products.php
@@ -12,8 +12,8 @@ $sql="
 SELECT
     p.id,
     typeID,
-    p.NAME,
-    pt.name AS prod,
+    p.name,
+    pt.name AS type,
     pg.name AS gr,
     p.image AS p_img,
     pt.image as pt_img,
@@ -28,19 +28,47 @@ LEFT JOIN productgr pg ON
 ";
 $arr = [];
 
-$result = $conn->query($sql);
+$sql_P = "
+SELECT * FROM `prodvsparam` 
+ORDER by `prodTypeID`";
 
+$result = $conn->query($sql);
+$result_P = $conn->query($sql_P);
  
+
+$arr_p = [];
+
+if (!$result_P){
+    die("SQL Error:\n" . $sql_P);
+}else{
+	if (mysqli_num_rows($result_P) > 0) {
+        foreach($result_P as $row){            
+            $typeID = $row["prodTypeID"];            
+            if (!isset($arr_p[$typeID])){                
+                $arr_p[$typeID] = [];                
+            }
+            array_push($arr_p[$typeID], $row["paramID"]);            
+        }
+    }
+}
+
 if (!$result){
     die("SQL Error:\n" . $sql);
 }else{
 	if (mysqli_num_rows($result) > 0) {
     
             foreach($result as $row){
+                $tp = $row["typeID"];
+                if (isset($arr_p[$tp])){
+                    $row["param"] = $arr_p[$tp];
+                }else{
+                    $row["param"] = [];
+                }                
             	$arr[] = $row;
             }
         }
 }
+
 
 echo json_encode($arr);
 
